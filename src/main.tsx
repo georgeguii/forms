bb4c698c578f797916652e95ba3a1588337e9260
George <george.gui2000@gmail.com>
2023-01-21 15:33:41 -0300

    fix: trying fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <App />
+    <HashRouter>
+        <App />
+    </HashRouter>
 )
