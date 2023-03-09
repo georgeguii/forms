ef402cd5fdf0701a44b9b5e09e2de205d707976e
George <george.gui2000@gmail.com>
2023-03-09 16:10:56 -0300

    fix: tentando corrigir logo no header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,18 +7,18 @@ export function Header() {
   // const navigate = useNavigate(); bg-gradient-to-t from-cyan-500 to-cyan-700
 
   return (
-    <header className="flex justify-between items-center h-[10vh] bg-[#c1d3ee]">
+    <header className="flex justify-between items-center h-[100px] bg-[#c1d3ee]">
       {/* ou h-24 */}
       {/* <span className="ml-10 text-white font-bold text-4xl">Icon</span> */}
       <img
         src={Logo}
-        className="ml-10 max-sm:ml-1 max-sm:max-w-[11rem] max-w-[14rem]"
+        className="ml-10 max-sm:ml-1 max-h-48"
         alt="Logo do projeto"
       />
 
       <img
         src={Uneb}
-        className="mr-10 max-sm:mr-2 max-w-[3.5rem]"
+        className="mr-10 max-sm:mr-2 max-h-20"
         alt="Logo do projeto"
       />
 
