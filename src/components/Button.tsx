ed4584b833aa0bfd13318168695d1a61e03124a4
George <george.gui2000@gmail.com>
2023-03-09 15:21:26 -0300

    fix: cores e add logo

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,12 +11,12 @@ export function Button({ value, enable = true }: ButtonProps) {
       type='submit'
       className='px-10 h-12 mt-5 rounded-md font-semibold
       flex justify-center items-center
-      text-gray-100
-      bg-cyan-600 hover:bg-cyan-7
+      text-slate-900
+      bg-[#a1b3d3] hover:bg-[#8fa1c7]
       00 
-      disabled:bg-cyan-500
+      disabled:bg-[#c1d3ee]
       transition-colors duration-300'
-      disabled = {!enable}
+      disabled={!enable}
     >
       {value.toUpperCase()}
     </button>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,31 @@
 // import { useNavigate } from "react-router";
+import Logo from "../assets/logo01.png"
+import Uneb from "../assets/uneb.png"
 
 export function Header() {
 
-  // const navigate = useNavigate();
+  // const navigate = useNavigate(); bg-gradient-to-t from-cyan-500 to-cyan-700
 
   return (
-    <header className="flex justify-between items-center h-[10vh] bg-gradient-to-t from-cyan-500 to-cyan-700">
+    <header className="flex justify-between items-center h-[10vh] bg-[#c1d3ee]">
       {/* ou h-24 */}
-      <span className="ml-10 text-white font-bold text-4xl">Icon</span>
+      {/* <span className="ml-10 text-white font-bold text-4xl">Icon</span> */}
+      <img
+        src={Logo}
+        className="ml-10 max-sm:ml-1 max-w-[14rem]"
+        alt="Logo do projeto"
+      />
+
+      <img
+        src={Uneb}
+        className="mr-10 max-sm:mr-2 max-w-[3.5rem]"
+        alt="Logo do projeto"
+      />
 
       {/* <button className='px-10 h-12 rounded-md font-semibold
       flex justify-center items-center
       text-gray-100
-      transition-colors duration-300' onClick={() => navigate("/baixar-excel")}>
+      transition-colors duration-300' onClick{() => navigate("/baixar-excel")}>
         Baixar Excel
       </button> */}
 
